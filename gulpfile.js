6e05bc9f468a86627a824461b7a3fd623ff9fad8
Gatlin Johnson <gatlin@niltag.net>
2016-04-29 13:09:40 -0500

    added gulp-concat dev dependency, single alm.js file now

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp')
-  , serve = require('gulp-serve');
+  , serve = require('gulp-serve')
+  , concat = require('gulp-concat');
 
 gulp.task('serve', serve('./'));
+
+gulp.task('scripts', function() {
+    return gulp.src(['./lib/loeb.js', './lib/alm.js'])
+        .pipe(concat('alm.js'))
+        .pipe(gulp.dest('./'));
+});
