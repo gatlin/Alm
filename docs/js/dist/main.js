e731055ae081bfaceb1e0b157671ed43e37e4725
Gatlin Johnson <gatlin@niltag.net>
2016-10-07 09:06:09 -0500

    working on tutorial-based site

--- docs/js/dist/main.js
+++ docs/js/dist/main.js
@@ -49,103 +49,30 @@
 	const alm = __webpack_require__(1),
 	      el = alm.el;
 
-	function rando(min,max) {
-	    return Math.floor(Math.random() * (max - min) + min);
-	}
-
+	/* A simple counter */
 	const app1 = new alm.App({
 	    eventRoot: 'app-1',
 	    domRoot: 'app-1',
-	    state: {
-	        words: ['ｗｏａｈ','ｗａｖｙ','ｔｕｂｕｌａｒ'],
-	        which: 0
-	    },
-	    update: (_, state) => {
-	        state.which = rando(0, state.words.length);
-	        return state;
-	    },
-	    render: state => el('div', {}, [
-	        el('p', {}, ['Click anywhere in this box for a random word']),
-	        el('p', {'class':'aesthetic'}, [ state.words[state.which]])
-	    ]),
+	    state: 0,
+	    update: (action, num) => num + (action ? 1 : -1),
+	    render: state =>
+	        el('div', { 'id':'app-1-main' }, [
+	            el('h3', { 'class':'app-1-header' }, [state.toString()]),
+	            el('span', {}, [
+	                el('button', { 'id':'up-btn' }, ['+']),
+	                el('button', { 'id':'down-btn' }, ['-'])
+	            ])
+	        ]),
 	    main: scope => {
-	        scope.events.click.connect(scope.actions);
-	    }
-	}).start();
-
-	function renderTasks(state) {
-	    const input = el('input', {
-	        'type': 'text',
-	        'id':'app-2-input',
-	        'placeholder':'Type something here go ahead'
-	    });
-
-	    const tasks_list = Object.keys(state.tasks)
-	          .map(taskId => {
-	              return el('li', {
-	                  'id':'app-2-task-'+taskId,
-	                  'class':'app-2-task'
-	              }, [state.tasks[taskId]]);
-	          });
-
-	    return el('div', {}, [
-	        input,
-	        el('p', {}, ['Click on an item to delete it']),
-	        el('ul', { 'id':'app-2-tasks' }, tasks_list)
-	    ]);
-	}
-
-	const app2 = new alm.App({
-	    eventRoot: 'app-2',
-	    domRoot: 'app-2',
-	    state: { id: 0, tasks: {}, num: 0 },
-	    update: (action, model) => {
-	        if (action.type === 'add') {
-	            const uid = model.id++;
-	            model.num++;
-	            model.tasks[uid] = action.data;
-	        }
-	        else if (action.type === 'del') {
-	            delete model.tasks[action.data];
-	            model.num--;
-	        }
-	        console.log(model);
-	        return model;
-	    },
-
-	    render: renderTasks,
-
-	    main: (scope) => {
-	        scope.events.keydown
-	            .filter(evt => evt.getRaw().keyCode === 13 &&
-	                    evt.getId() === 'app-2-input')
-	            .recv(evt => scope.actions.send({
-	                type: 'add',
-	                data: evt.getRaw().target.value
-	            }));
+	        scope.events.click
+	            .filter(evt => evt.getId() === 'up-btn')
+	            .recv(evt => scope.actions.send(true));
 
 	        scope.events.click
-	            .filter(evt => evt.hasClass('app-2-task'))
-	            .recv(evt => scope.actions.send({
-	                type: 'del',
-	                data: evt.getId().split('-')[3]
-	            }));
-
-	        scope.state
-	            .map(model => (model.num
-	                           ? '('+model.num.toString()+') '
-	                           : '') + 'Alm')
-	            .connect(scope.ports.outbound.title);
-	    },
-	    ports: {
-	        outbound: ['title']
+	            .filter(evt => evt.getId() === 'down-btn')
+	            .recv(evt => scope.actions.send(false));
 	    }
 	}).start();
-
-	app2.ports.outbound.title
-	    .recv(title => {
-	        document.title = title;
-	    });
 
 
 /***/ },
@@ -299,6 +226,30 @@
 	         */
 	        App.prototype.editScope = function (cb) {
 	            cb(this.scope);
+	            return this;
+	        };
+	        /**
+	         * Set the root element in the page to which we will attach listeners.
+	         * @param er - Either an HTML element, the whole document, or an element ID
+	         *             as a string.
+	         * @return @this
+	         */
+	        App.prototype.setEventRoot = function (er) {
+	            this.eventRoot = typeof er === 'string'
+	                ? document.getElementById(er)
+	                : er;
+	            return this;
+	        };
+	        /**
+	         * Set the root element in the page in which we will render.
+	         * @param er - Either an HTML element, the whole document, or an element ID
+	         *             as a string.
+	         * @return @this
+	         */
+	        App.prototype.setDomRoot = function (dr) {
+	            this.domRoot = typeof dr === 'string'
+	                ? document.getElementById(dr)
+	                : dr;
 	            return this;
 	        };
 	        /**
--- docs/js/src/main.js
+++ docs/js/src/main.js
@@ -3,100 +3,27 @@
 const alm = require('../../../dist/lib/alm.js'),
       el = alm.el;
 
-function rando(min,max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
+/* A simple counter */
 const app1 = new alm.App({
     eventRoot: 'app-1',
     domRoot: 'app-1',
-    state: {
-        words: ['ｗｏａｈ','ｗａｖｙ','ｔｕｂｕｌａｒ'],
-        which: 0
-    },
-    update: (_, state) => {
-        state.which = rando(0, state.words.length);
-        return state;
-    },
-    render: state => el('div', {}, [
-        el('p', {}, ['Click anywhere in this box for a random word']),
-        el('p', {'class':'aesthetic'}, [ state.words[state.which]])
-    ]),
+    state: 0,
+    update: (action, num) => num + (action ? 1 : -1),
+    render: state =>
+        el('div', { 'id':'app-1-main' }, [
+            el('h3', { 'class':'app-1-header' }, [state.toString()]),
+            el('span', {}, [
+                el('button', { 'id':'up-btn' }, ['+']),
+                el('button', { 'id':'down-btn' }, ['-'])
+            ])
+        ]),
     main: scope => {
-        scope.events.click.connect(scope.actions);
-    }
-}).start();
-
-function renderTasks(state) {
-    const input = el('input', {
-        'type': 'text',
-        'id':'app-2-input',
-        'placeholder':'Type something here go ahead'
-    });
-
-    const tasks_list = Object.keys(state.tasks)
-          .map(taskId => {
-              return el('li', {
-                  'id':'app-2-task-'+taskId,
-                  'class':'app-2-task'
-              }, [state.tasks[taskId]]);
-          });
-
-    return el('div', {}, [
-        input,
-        el('p', {}, ['Click on an item to delete it']),
-        el('ul', { 'id':'app-2-tasks' }, tasks_list)
-    ]);
-}
-
-const app2 = new alm.App({
-    eventRoot: 'app-2',
-    domRoot: 'app-2',
-    state: { id: 0, tasks: {}, num: 0 },
-    update: (action, model) => {
-        if (action.type === 'add') {
-            const uid = model.id++;
-            model.num++;
-            model.tasks[uid] = action.data;
-        }
-        else if (action.type === 'del') {
-            delete model.tasks[action.data];
-            model.num--;
-        }
-        console.log(model);
-        return model;
-    },
-
-    render: renderTasks,
-
-    main: (scope) => {
-        scope.events.keydown
-            .filter(evt => evt.getRaw().keyCode === 13 &&
-                    evt.getId() === 'app-2-input')
-            .recv(evt => scope.actions.send({
-                type: 'add',
-                data: evt.getRaw().target.value
-            }));
+        scope.events.click
+            .filter(evt => evt.getId() === 'up-btn')
+            .recv(evt => scope.actions.send(true));
 
         scope.events.click
-            .filter(evt => evt.hasClass('app-2-task'))
-            .recv(evt => scope.actions.send({
-                type: 'del',
-                data: evt.getId().split('-')[3]
-            }));
-
-        scope.state
-            .map(model => (model.num
-                           ? '('+model.num.toString()+') '
-                           : '') + 'Alm')
-            .connect(scope.ports.outbound.title);
-    },
-    ports: {
-        outbound: ['title']
+            .filter(evt => evt.getId() === 'down-btn')
+            .recv(evt => scope.actions.send(false));
     }
 }).start();
-
-app2.ports.outbound.title
-    .recv(title => {
-        document.title = title;
-    });
