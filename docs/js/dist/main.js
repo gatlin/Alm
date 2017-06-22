8248654699646c5786a28f46ac25d8ed0bc5098c
Gatlin Johnson <gatlin@niltag.net>
2017-06-22 12:07:56 -0500

    how does git manage to automatically undo stuff while i sleep

--- docs/js/dist/main.js
+++ docs/js/dist/main.js
@@ -281,7 +281,7 @@
 	            // create the signal graph
 	            var actions = new base_2.Mailbox(null);
 	            var state = actions.reduce(cfg.state, function (action, model) {
-	                if (action) {
+	                if (action !== null) {
 	                    return cfg.update(action, model);
 	                }
 	                return model;
