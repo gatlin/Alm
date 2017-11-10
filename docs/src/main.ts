a2c7f418d09f93dd59506928da89106b75627ed4
Gatlin Johnson <gatlin@niltag.net>
2017-11-09 18:09:20 -0600

    updating the examples page

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -5,54 +5,89 @@ import {
     Alm
 } from '../../src/alm';
 
-type State = {
+const CounterComponent = ({ counter, increment, decrement }) =>
+    el('div', {}, [
+        el('p', {}, [counter.toString()]),
+        el('div', {}, [
+            el('button', { on: { click: evt => increment() } },
+                ['Increment']),
+            el('button', { on: { click: evt => decrement() } },
+                ['Decrement'])
+        ])
+    ]);
+
+const CounterView = connect(
+    counter => ({ counter }),
+    dispatch => ({
+        increment: () => dispatch({ type: true }),
+        decrement: () => dispatch({ type: false })
+    })
+)(CounterComponent);
+
+const counterApp = new Alm({
+    initialState: 0,
+    reducer: (state, action) => action.type ? state + 1 : state - 1,
+    view: CounterView(),
+    domRoot: 'counter-app',
+    eventRoot: 'counter-app'
+});
+
+counterApp.start();
+
+// EVENT example
+
+type EventState = {
+    count: number;
+    overLimit: false;
     inputText: string;
 };
 
-enum Actions {
+enum EventActions {
     UpdateText
 };
 
-const updateTextAction = data => ({
-    type: Actions.UpdateText,
-    data
-});
+const eventReducer = (state, action) => {
+    switch (action.type) {
+        case EventActions.UpdateText: {
+            let inputText = action.data;
+            return {
+                inputText,
+                count: inputText.length,
+                overLimit: inputText.length > 140
+            };
+        }
+        default:
+            return state;
+    };
+};
 
-const reducer = makeReducer({
-    inputText: (state: string, action) => {
-        switch (action.type) {
-            case Actions.UpdateText:
-                return action.data;
-            default:
-                return state;
-        };
-    }
-});
-
-const MainComponent = ({ inputText, updateText }) =>
-    el('div', { 'id': 'the-app' }, [
-        el('h1', {}, ['Alm']),
-        el('input', {
-            type: 'text',
-            value: inputText,
+const EventComponent = ({ inputText, count, overLimit, updateText }) =>
+    el('div', {}, [
+        el('textarea', {
+            id: 'text-event',
             on: {
                 input: evt => updateText(evt.getValue())
             }
         }),
-        el('p', {}, [inputText])
+        el('p', {}, [count.toString() + ' / 140 characters'])
     ]);
 
-const MainView = connect(
-    ({ inputText }) => ({ inputText }),
+const EventView = connect(
+    state => state,
     dispatch => ({
-        updateText: t => dispatch(updateTextAction(t))
+        updateText: data => dispatch({
+            type: EventActions.UpdateText,
+            data
+        })
     })
-)(MainComponent);
+)(EventComponent);
 
-const app = new Alm<State, Actions>({
-    initialState: { inputText: '' },
-    reducer,
-    view: MainView(),
-    domRoot: 'app-root'
+const eventApp = new Alm({
+    initialState: { inputText: '', count: 0, overLimit: false },
+    reducer: eventReducer,
+    view: EventView(),
+    eventRoot: 'event-app',
+    domRoot: 'event-app'
 });
-app.start();
+
+eventApp.start();
